3bc3c090bb1fdbe4fbfe05429d09fdef3b137712
e7240m2 <matt@mttmrtn.home>
2020-02-16 16:34:51 -0500

    blog implemented

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,41 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const postTemplate = path.resolve("src/templates/blogpost.js")
+
+  return graphql(`
+    query BlogIndexQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              author
+              date
+              path
+              tags
+              title
+            }
+            html
+            id
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+      })
+    })
+  })
+}
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `coral`,
+      background: `darkblue`,
       marginBottom: `none`,
     }}
   >
