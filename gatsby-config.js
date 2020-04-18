c4c6e95fa1fcc94c0b1915a5f8f7c649a42bfedf
matt martin <matt@angryjenkins.com>
2020-04-18 14:07:07 -0400

    possible angryjenkins site @font-face StiffStaff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,45 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Crash Course`,
-    description: `Learn to make and deploy a basic Gatsby site with Brad Traversy.`,
-    author: `@mttmrtn`,
-  },
-  plugins: [
-    'gatsby-plugin-sass',
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `angryjenkins`,
+        description: `Matt Matin's Adventures in New Media.`,
+        author: `@angryjenkins`,
+        keywords: ['angryjenkins','Matt Martin', 'Matthew Martin', 'web development','reactjs','angularjs','gatsbyjs','javascript','nodejs','new media', 'digital', 'front end','linux','ux','nyc'],
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages/blog`,
-        name: 'blog'
-      }
-    },
-    'gatsby-transformer-remark',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        'gatsby-plugin-sass',
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/ajlogo.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-catch-links`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages/blog`,
+                name: 'blog',
+            },
+        },
+        'gatsby-transformer-remark',
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,52 +1,52 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const BlogPage = ({ data }) => {
-  return (
-    <Layout>
-      <SEO title="Blog" />
-      <h1>Latest Posts:</h1>
-      {data.allMarkdownRemark.edges.map(post => (
-        <div key={post.node.id}>
-          <h3>{post.node.frontmatter.title}</h3>
-          <small>
-            posted by {post.node.frontmatter.author} on{" "}
-            {post.node.frontmatter.date}
-          </small>
+    return (
+        <Layout>
+            <SEO title="Blog" />
+            <h1>Latest Posts:</h1>
+            {data.allMarkdownRemark.edges.map(post => (
+                <div key={post.node.id}>
+                    <h3>{post.node.frontmatter.title}</h3>
+                    <small>
+                        posted by {post.node.frontmatter.author} on{' '}
+                        {post.node.frontmatter.date}
+                    </small>
 
-          <hr />
+                    <hr />
 
-          <Link to={post.node.frontmatter.path}>Read More</Link>
-          <br />
-          <br />
-          <br />
-        </div>
-      ))}
-    </Layout>
-  )
+                    <Link to={post.node.frontmatter.path}>Read More</Link>
+                    <br />
+                    <br />
+                    <br />
+                </div>
+            ))}
+        </Layout>
+    )
 }
 
 export const pageQuery = graphql`
-  query BlogIndexQuery {
-    allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            author
-            date(formatString: "DD MMMM, YYYY")
-            path
-            tags
-            title
-          }
-          excerpt
-          id
+    query BlogIndexQuery {
+        allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        author
+                        date(formatString: "DD MMMM, YYYY")
+                        path
+                        tags
+                        title
+                    }
+                    excerpt
+                    id
+                }
+            }
         }
-      }
     }
-  }
 `
 
 export default BlogPage
